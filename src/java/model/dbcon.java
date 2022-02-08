fc2382c1075694b66f8fdfa6e6094859cda07707
Hiruna Gayashan <64424930+Hirunagrad@users.noreply.github.com>
2022-02-09 00:35:45 +0530

    hend

--- src/java/model/dbcon.java
+++ src/java/model/dbcon.java
@@ -5,10 +5,52 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 /**
  *
  * @author hirun
  */
 public class dbcon {
-    
+    static String newLine = System.getProperty("line.separator");
+
+    volatile static dbcon s;
+
+    public static Connection conn; 
+    public static Statement stmt = null;
+
+    private dbcon() throws SQLException {
+        // create single instance of DB connection 
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "root");
+        connectionProps.put("password", "");
+        // port & db name 
+        conn = DriverManager.getConnection("jdbc:mysql://"
+                + "localhost" + ":" + "3306" + "/" + "hotel",
+                connectionProps);
+    }
+
+    public Connection getConnection() throws SQLException {
+        return conn; 
+    }
+
+    public static dbcon getInstance() throws SQLException {
+        dbcon s = dbcon.s;
+
+        if (s == null) {
+            synchronized(dbcon.class) {
+                s = dbcon.s;
+                if (s == null) {
+                    dbcon.s = s = new dbcon(); 
+                    System.out.print("First DB connection created." + newLine); 
+            }
+        }
+    }
+        conn = s.getConnection();
+        return s; 
+    }
 }
